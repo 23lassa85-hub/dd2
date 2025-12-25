6fbc530e72ffe42fc6fb549aa5ce8f5e3260d057
23lassa85-hub <23lassa85@gmail.com>
2025-12-25 23:55:41 +0200

    rrre

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите строку:");
+string text = Console.ReadLine();
+
+char[] mas = text.ToCharArray();
+
+int k1 = 0;
+int k2 = 0;
+
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i] == '(' | mas[i] == ')') k1++;
+    if (mas[i] == '[' | mas[i] == ']') k2++;
+}
+
+Console.WriteLine("Круглых скобок: " + k1);
+Console.WriteLine("Квадратных скобок: " + k2);
